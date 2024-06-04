260984adf1954786a636f0161660cadbdf0b648b
Justin <JustinE@tlgcohort.com>
2024-06-04 09:58:52 -0500

    Lab complete including use of Files class

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -9,6 +9,8 @@
 package com.poetry.io;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class PoemClient {
 
@@ -17,7 +19,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         readPoem();
-        writePoem();
+        //writePoem();
     }
 
     /**
@@ -33,17 +35,26 @@ public class PoemClient {
      * The try-with-resources below allows you to initialize the stream and auto-close it.
      */
     private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        //try-with-resources
-        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
-            String line;
-            while((line = reader.readLine()) != null){  //when is goes null, you're at EOF (end of file)
-                System.out.println(line);
-            }
-        }
-        catch (IOException e) {
+
+        //More modern and easier way to read files!!!
+        try {
+            String poem = Files.readString(Path.of("famous-poem.txt"));
+            System.out.println(poem);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        //try-with-resources
+//        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+//            String line;
+//            while((line = reader.readLine()) != null){  //when is goes null, you're at EOF (end of file)
+//                System.out.println(line);
+//            }
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
     }
 
     /**
